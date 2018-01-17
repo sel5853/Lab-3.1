cba21bb3dc899a890db25f8d061e13684637453e
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-17 09:23:27 -0500

    .

--- Lab 3.1/src/ArrayMethods1.java
+++ Lab 3.1/src/ArrayMethods1.java
@@ -1,4 +1,26 @@
 
 public class ArrayMethods1 {
 
+	public static void insertionSort(int[] list1)
+	{
+		
+	}
+	
+	public static void selectionSort(double[] list1)
+	{
+		
+	}
+	
+	public static void bubbleSort(String[] list1)
+	{
+		
+	}
+	
+	public static void swap(int[] arr, int index1, int index2)
+	{
+		int x = index1;
+		arr[index1] = arr[index2];
+		arr[index2] = x;
+	}
+	
 }
