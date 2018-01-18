22ce9f123dec82832095b1067c4af76170213ecc
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-18 09:23:16 -0500

    .

--- Lab 3.1/src/ArrayMethods1.java
+++ Lab 3.1/src/ArrayMethods1.java
@@ -13,10 +13,48 @@ public class ArrayMethods1 {
 	
 	public static void bubbleSort(String[] list1)
 	{
+		for(int x = 0;  x < list1.length; x++)
+		{
+			String a = list1[x];
+			String b = list1[x+1];
+			if()
+			{
+				swapString(list1[x], a, b);
+			}
+		}
+	}
+	
+	private static void swapString(String x, String a, String b)
+	{
 		
 	}
 	
-	public static void swap(int[] arr, int index1, int index2)
+	private static void swapInt(int x, int a, int b)
+	{
+		
+	}
+	
+	private static void swapDouble(double x, double a, double b)
+	{
+		
+	}
+
+	public static void swapInt(int[] arr, int index1, int index2)
+	{
+		int x = index1;
+		arr[index1] = arr[index2];
+		arr[index2] = x;
+	}
+	
+	public static void swapString(String[] arr, int index1, int index2)
+	{
+		int x = index1;
+		String a = arr[x];
+		arr[index1] = arr[index2];
+		arr[index2] = a;
+	}
+	
+	public static void swapDouble(double[] arr, int index1, int index2)
 	{
 		int x = index1;
 		arr[index1] = arr[index2];
